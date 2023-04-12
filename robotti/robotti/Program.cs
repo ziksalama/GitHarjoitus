02c5879e8948c1db42ab20420fd032ba4c223bb9
Eino <eino@eino.one>
2023-04-12 10:26:35 +0300

    robotti

---
+++ robotti/robotti/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+
+public interface IRobottiKäsky
+{
+    void Suorita(Robotti robotti);
+}
+
+public class Robotti
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+    public bool OnKäynnissä { get; set; }
+    public IRobottiKäsky?[] Käskyt { get; } = new IRobottiKäsky?[3];
+
+    public void Suorita()
+    {
+        foreach (IRobottiKäsky? käsky in Käskyt)
+        {
+            käsky?.Suorita(this);
+            Console.WriteLine($"[{X} {Y} {OnKäynnissä}]");
+        }
+    }
+}
+
+public class Käynnistä : IRobottiKäsky
+{
+    public void Suorita(Robotti robotti)
+    {
+        robotti.OnKäynnissä = true;
+    }
+}
+
+public class Sammuta : IRobottiKäsky
+{
+    public void Suorita(Robotti robotti)
+    {
+        robotti.OnKäynnissä = false;
+    }
+}
+
+public class YlösKäsky : IRobottiKäsky
+{
+    public void Suorita(Robotti robotti)
+    {
+        if (robotti.OnKäynnissä)
+        {
+            robotti.Y++;
+        }
+    }
+}
+
+public class AlasKäsky : IRobottiKäsky
+{
+    public void Suorita(Robotti robotti)
+    {
+        if (robotti.OnKäynnissä)
+        {
+            robotti.Y--;
+        }
+    }
+}
+
+public class VasenKäsky : IRobottiKäsky
+{
+    public void Suorita(Robotti robotti)
+    {
+        if (robotti.OnKäynnissä)
+        {
+            robotti.X--;
+        }
+    }
+}
+
+public class OikeaKäsky : IRobottiKäsky
+{
+    public void Suorita(Robotti robotti)
+    {
+        if (robotti.OnKäynnissä)
+        {
+            robotti.X++;
+        }
+    }
+}
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Robotti robotti = new Robotti();
+
+        for (int i = 0; i < 3; i++)
+        {
+            Console.WriteLine("Anna käsky (Käynnistä, Sammuta, Ylös, Alas, Vasen, Oikea):");
+            string käsky = Console.ReadLine();
+
+            switch (käsky.ToLower())
+            {
+                case "käynnistä":
+                    robotti.Käskyt[i] = new Käynnistä();
+                    break;
+                case "sammuta":
+                    robotti.Käskyt[i] = new Sammuta();
+                    break;
+                case "ylös":
+                    robotti.Käskyt[i] = new YlösKäsky();
+                    break;
+                case "alas":
+                    robotti.Käskyt[i] = new AlasKäsky();
+                    break;
+                case "vasen":
+                    robotti.Käskyt[i] = new VasenKäsky();
+                    break;
+                case "oikea":
+                    robotti.Käskyt[i] = new OikeaKäsky();
+                    break;
+                default:
+                    Console.WriteLine("Tuntematon käsky.");
+                    i--;
+                    break;
+            }
+        }
+
+        robotti.Suorita();
+
+        Console.ReadLine();
+    }
+}
